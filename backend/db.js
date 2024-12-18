84eb857a15f52b784fdf3878f6146230c20a8184
RestiFauziM <restimukhliza17@gmail.com>
2024-12-18 14:58:47 +0700

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -10,7 +10,7 @@ const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '',
-    database: 'signup',
+    database: 'controlmoney',
 });
 
 db.connect((err) => {
